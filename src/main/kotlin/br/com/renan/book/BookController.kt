b87846aa6b0b01ac36a28cee2c6da427f72ccdab
Renan Martins <renantins@outlook.com>
2022-01-02 18:21:31 -0300

    More methods

--- src/main/kotlin/br/com/renan/book/BookController.kt
+++ src/main/kotlin/br/com/renan/book/BookController.kt
@@ -3,22 +3,32 @@ package br.com.renan.book
 import org.springframework.web.bind.annotation.*
 
 @RestController
-@RequestMapping("/book")
+@RequestMapping("/books")
 class BookController(var bookRepository: BookRepository) {
 
     @GetMapping()
-    fun getAllBooks() : List<Book> {
-       return bookRepository.findAll();
+    fun getAllBooks(): List<Book> {
+        return bookRepository.findAll();
     }
 
     @PostMapping
-    fun saveBook(@RequestBody book : Book) {
+    fun saveBook(@RequestBody book: Book) {
+        bookRepository.save(book)
+    }
+
+    @PutMapping
+    fun updateBook(@RequestBody book: Book) {
         bookRepository.save(book)
     }
 
     @GetMapping("/{id}")
-    fun getBookById(@PathVariable id : Long) : Book {
+    fun getBookById(@PathVariable id: Long): Book {
         return bookRepository.findById(id).get()
+    }
+
+    @DeleteMapping("/{id}")
+    fun deleteBookById(@PathVariable id: Long) {
+        bookRepository.deleteById(id)
     }
 
 }
