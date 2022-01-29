1469d3bcd0227cb178687d46a6a826e2a8691b0b
Renan Martins <renantins@outlook.com>
2022-01-29 12:12:22 -0300

    Add unit test

--- src/test/kotlin/br/com/renan/book/BookApplicationTests.kt
+++ src/test/kotlin/br/com/renan/book/BookApplicationTests.kt
@@ -1,13 +1,37 @@
 package br.com.renan.book
 
+import br.com.renan.book.domain.Book
+import br.com.renan.book.repository.BookRepository
+import br.com.renan.book.service.BookService
+import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.Test
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito
 import org.springframework.boot.test.context.SpringBootTest
+import java.util.*
 
 @SpringBootTest
-class BookApplicationTests {
+class BookApplicationTests{
+
+    @InjectMocks
+    lateinit var bookService: BookService
+    @Mock
+    lateinit var bookRepository: BookRepository
+
 
     @Test
-    fun contextLoads() {
+    fun givenBookIdShouldReturAnBook() {
+        val book = Book("Uncle Bob", "Tech", "Clean Code", "Unknow")
+        book.id = 1L
+        val optionalBook = Optional.of(book)
+
+        Mockito.`when`(bookRepository.findById(1L)).thenReturn(optionalBook)
+
+        val current = bookService.findById(1L)
+
+        Assertions.assertThat(current.get().author).isEqualTo(book.author)
     }
 
 }
+
